d11a843460dc22f808dc1b232540d960c107b306
Prasoly <maksim.tsernjakov@gmail.com>
2025-12-02 14:36:22 +0200

    fixed

--- tests/no_users_controllers.spec.ts
+++ tests/no_users_controllers.spec.ts
@@ -8,16 +8,14 @@ test("should return empty array when no users exist", async ({ request }) => {
     const response = await request.get(baseURLWithEndpoint);
     expect(response.status()).toBe(StatusCodes.OK);
 
-    const responseBody = await response.text()
-    expect(responseBody).toBe('[]');
+    const responseBody = await response.text();
+    expect(JSON.parse(responseBody)).toEqual([]);
 });
-
 
 test("should return 404 if deleting non-existent user", async ({ request }) => {
     const response = await request.delete(`${baseURLWithEndpoint}/1`);
     expect(response.status()).toBe(StatusCodes.NOT_FOUND);
 });
-
 
 test("should return 404 if user not found", async ({ request }) => {
     const response = await request.get(`${baseURLWithEndpoint}/1`);
